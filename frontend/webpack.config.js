bc44eaff9683598b5609620699edc5a8e23531c6
leo <longshuang@msn.cn>
2019-06-11 20:18:40 +0800

    删除webpack-dev-server的依赖

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,28 +12,7 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
-//    devtool: 'inline-source-map',
-    devServer: {
-        historyApiFallback: true,
-        before: function (app, server) {
-            app.get('/', (req, rsp) => {
-                rsp.writeHead(200, {'Content-Type': 'text/html'});
-                rsp.end(`<!DOCTYPE html>
-                    <html>
-                        <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <title>模拟 - 协作系统</title>
-                            <link rel="shortcut icon" href="./dist/favicon.ico" />
-                        </head>
-                        <body>
-                            <div id="app"></div>                
-                            <script src="./app.bundle.js"></script>
-                        </body>
-                    </html>`);
-            });
-        }
-    },
+//  devtool: 'inline-source-map',
     module: {
         rules: [
             {
