9c0f32066dce730ed5709b502a50b0e9ecf41c1c
leo <longshuang@msn.cn>
2019-06-13 17:59:38 +0800

    还是用ts-loader吧

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'awesome-typescript-loader',
+                    loader: 'ts-loader',
                     options: {
                         getCustomTransformers: () => ({
                             before: [
