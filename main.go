3daad2cf3035460c1bb136d533a8165759e282b8
leo <longshuang@msn.cn>
2020-03-13 11:08:48 +0800

    调整代码

--- main.go
+++ main.go
@@ -17,32 +17,36 @@ func main() {
 	// Load configuration.
 	config.Default.Load()
 
-	// Open database
+	// Open database.
 	if config.Default.Installed {
 		if err := orm.OpenDB("mysql", config.Default.GetMySQLAddr()); err != nil {
 			web.Logger.Fatal("Failed to connect to database: %s. Reason: %v", config.Default.GetMySQLAddr(), err)
 		}
 	}
 
-	// Create router for httpd service
+	// Load resources.
+	resBox := rice.MustFindBox("view/dist")
+	mainPage := strings.ReplaceAll(resBox.MustString("app.html"), "__APP_NAME__", config.Default.AppName)
+
+	// Create router for httpd service.
 	router := web.NewRouter()
 	router.Use(middleware.Logger)
 	router.Use(middleware.PanicAsError)
 
-	// Resources.
-	router.SetPage("/", strings.ReplaceAll(rice.MustFindBox("view/dist").MustString("app.html"), "__APP_NAME__", config.Default.AppName))
-	router.StaticFS("/assets", rice.MustFindBox("view/dist").HTTPBox())
+	// Statics.
+	router.SetPage("/", mainPage)
+	router.StaticFS("/assets", resBox.HTTPBox())
 	router.StaticFS("/uploads", web.Dir("uploads"))
 
-	// Home/Login/Logout
+	// Home. Check display mode(Install? Login? Normal?)
 	router.GET("/home", controller.Home)
-	router.GET("/logout", controller.Logout)
-	router.POST("/login", controller.Login)
 
-	// Install
+	// Install/Login/Logout API.
 	router.UseController("/install", new(controller.Install), middleware.MustNotInstalled)
+	router.GET("/logout", controller.Logout, middleware.MustInstalled)
+	router.POST("/login", controller.Login, middleware.MustInstalled)
 
-	// Normal API
+	// Normal API.
 	api := router.Group("/api")
 	api.Use(middleware.MustInstalled)
 	api.Use(middleware.AutoLogin)
@@ -54,7 +58,7 @@ func main() {
 	api.UseController("/file", new(controller.File))
 	api.UseController("/notice", new(controller.Notice))
 
-	// Admin API
+	// Admin API.
 	router.UseController(
 		"/admin",
 		new(controller.Admin),
